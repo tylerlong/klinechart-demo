054d77f1edd9dd5377a95b96f4afd8b2087976a2
Tyler Liu <tyler4long@gmail.com>
2024-01-15 20:12:29 -0800

    Refactor code for financials

--- src/financials.tsx
+++ src/financials.tsx
@@ -1,43 +1,31 @@
 import type { IStockFinancialResults } from '@polygon.io/client-js';
 import { Descriptions } from 'antd';
-import type { DescriptionsItemType } from 'antd/es/descriptions';
 import React from 'react';
 import { formatNumber } from './utils';
 
 export const financial = (r: IStockFinancialResults) => {
   const results: React.JSX.Element[] = [];
-  const balanceSheet = r.results?.[0].financials.balance_sheet;
-  if (balanceSheet) {
-    const items: DescriptionsItemType[] = [];
-    if (balanceSheet.assets) {
-      items.push({
-        label: balanceSheet.assets.label,
-        key: balanceSheet.assets.label,
-        children: `${formatNumber(balanceSheet.assets.value)} ${balanceSheet.assets.unit}`,
-      });
-    }
-    if (balanceSheet.current_assets) {
-      items.push({
-        label: balanceSheet.current_assets.label,
-        key: balanceSheet.current_assets.label,
-        children: `${formatNumber(balanceSheet.current_assets.value)} ${balanceSheet.current_assets.unit}`,
-      });
-    }
-    if (balanceSheet.current_liabilities) {
-      items.push({
-        label: balanceSheet.current_liabilities.label,
-        key: balanceSheet.current_liabilities.label,
-        children: `${formatNumber(balanceSheet.current_liabilities.value)} ${balanceSheet.current_liabilities.unit}`,
-      });
-    }
-    if (balanceSheet.equity) {
-      items.push({
-        label: balanceSheet.equity.label,
-        key: balanceSheet.equity.label,
-        children: `${formatNumber(balanceSheet.equity.value)} ${balanceSheet.equity.unit}`,
-      });
-    }
+  const balance_sheet = r.results?.[0].financials.balance_sheet;
+  if (balance_sheet) {
+    const items = Object.values(balance_sheet)
+      .sort((a, b) => a.order - b.order)
+      .map(({ label, value, unit }) => ({
+        label,
+        key: label,
+        children: `${formatNumber(value)} ${unit}`,
+      }));
     results.push(<Descriptions key="balance_sheet" title="Blance Sheet" items={items} />);
+  }
+  const cash_flow_statement = r.results?.[0].financials.cash_flow_statement;
+  if (cash_flow_statement) {
+    const items = Object.values(cash_flow_statement)
+      .sort((a, b) => a.order - b.order)
+      .map(({ label, value, unit }) => ({
+        label,
+        key: label,
+        children: `${formatNumber(value)} ${unit}`,
+      }));
+    results.push(<Descriptions key="cash_flow_statement" title="Cash Flow Statement" items={items} />);
   }
   return results;
 };
