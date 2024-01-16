82bf8cebac599f22f027164feba5fee95fcb0790
Tyler Liu <tyler4long@gmail.com>
2024-01-15 16:05:18 -0800

    Use polygon SDK

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Button, Input, Space, Typography, Divider } from 'antd';
 import { auto } from 'manate/react';
 import { init, dispose } from 'klinecharts';
+import type { ITickerDetails, ITickerNews } from '@polygon.io/client-js';
 
 import type { Store } from './store';
-import type { TickerDetail, TickerNews } from './types';
 import polygon from './polygon';
 
 const { Title, Paragraph } = Typography;
@@ -15,8 +15,8 @@ const App = (props: { store: Store }) => {
   const [timespan, setTimespan] = useState('day');
   const [from, setFrom] = useState('2000-01-01');
   const [to, setTo] = useState('2030-01-01');
-  const [ticketDetail, setTickerDetail] = useState(undefined as TickerDetail | undefined);
-  const [tickerNews, setTickerNews] = useState([] as TickerNews[]);
+  const [ticketDetail, setTickerDetail] = useState(undefined as ITickerDetails | undefined);
+  const [tickerNews, setTickerNews] = useState(undefined as ITickerNews | undefined);
   const render = () => (
     <>
       <Title>KLineChart Demo</Title>
@@ -31,34 +31,30 @@ const App = (props: { store: Store }) => {
             // clean
             dispose('chart');
             setTickerDetail(undefined);
-            setTickerNews([]);
+            setTickerNews(undefined);
 
             // chart
             const chart = init('chart', { timezone: 'America/New_York', locale: 'en-US' })!;
-            const aggregatesBars = await polygon.aggregatesBars({
-              ticker,
-              multiplier: parseInt(multiplier, 10),
-              timespan,
-              from,
-              to,
+            const aggs = await polygon.stocks.aggregates(ticker, parseInt(multiplier, 10), timespan, from, to, {
+              limit: 50000,
             });
             chart.applyNewData(
-              aggregatesBars.map((ab) => ({
-                timestamp: ab.t,
-                open: ab.o,
-                high: ab.h,
-                low: ab.l,
-                close: ab.c,
-                volume: ab.v,
-                turnover: ab.vw,
+              aggs.results!.map((item) => ({
+                timestamp: item.t!,
+                open: item.o!,
+                high: item.h!,
+                low: item.l!,
+                close: item.c!,
+                volume: item.v,
+                turnover: item.vw,
               })),
             );
 
             // ticker detail
-            setTickerDetail(await polygon.tickerDetail({ ticker }));
+            setTickerDetail(await polygon.reference.tickerDetails(ticker));
 
             // ticker news
-            setTickerNews(await polygon.tickerNews({ ticker }));
+            setTickerNews(await polygon.reference.tickerNews({ ticker, limit: 100 }));
           }}
         >
           Apply
@@ -69,25 +65,27 @@ const App = (props: { store: Store }) => {
 
       <Divider />
 
-      {ticketDetail && (
+      {ticketDetail?.results && (
         <>
-          <Title level={2}>{ticketDetail.name}</Title>
+          <Title level={2}>{ticketDetail.results?.name}</Title>
           <Paragraph>
-            {ticketDetail.branding.icon_url && (
+            {ticketDetail.results?.branding?.icon_url && (
               <img
-                src={`${ticketDetail.branding.icon_url}?apiKey=${process.env.POLYGON_API_KEY}`}
-                alt={`${ticketDetail.name} icon`}
+                src={`${ticketDetail.results.branding.icon_url}?apiKey=${process.env.POLYGON_API_KEY}`}
+                alt={`${ticketDetail.results.name} icon`}
                 className="inline-image"
               />
             )}
-            {ticketDetail.description}
+            {ticketDetail.results?.description}
           </Paragraph>
         </>
       )}
 
+      <Divider />
+
       <Paragraph>
         <ul>
-          {tickerNews.map((n) => (
+          {tickerNews?.results.map((n) => (
             <li key={n.id}>
               <Space>
                 <a href={n.article_url} target="_blank">
--- src/polygon.ts
+++ src/polygon.ts
@@ -1,44 +1,5 @@
-import type { AxiosInstance } from 'axios';
-import axios from 'axios';
+import { restClient } from '@polygon.io/client-js';
 
-import type {
-  AggregatesBar,
-  AggregatesBarsParams,
-  TickerDetail,
-  TickerDetailsParams,
-  TickerNews,
-  TickerNewsParams,
-} from './types';
+const polygon = restClient(process.env.POLYGON_API_KEY!);
 
-class Polygon {
-  private axios: AxiosInstance;
-  public constructor(apiKey: string) {
-    this.axios = axios.create({
-      baseURL: 'https://api.polygon.io',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-  }
-
-  public async aggregatesBars(params: AggregatesBarsParams): Promise<AggregatesBar[]> {
-    const r = await this.axios.get(
-      `/v2/aggs/ticker/${params.ticker}/range/${params.multiplier}/${params.timespan}/${params.from}/${params.to}`,
-      { params: { limit: 50000 } },
-    );
-    return r.data.results;
-  }
-
-  public async tickerDetail(params: TickerDetailsParams): Promise<TickerDetail> {
-    const r = await this.axios.get(`/v3/reference/tickers/${params.ticker}`);
-    return r.data.results;
-  }
-
-  public async tickerNews(params: TickerNewsParams): Promise<TickerNews[]> {
-    const r = await this.axios.get(`/v2/reference/news?ticker=${params.ticker}`, { params: { limit: 1000 } });
-    return r.data.results;
-  }
-}
-
-const polygon = new Polygon(process.env.POLYGON_API_KEY!);
 export default polygon;
