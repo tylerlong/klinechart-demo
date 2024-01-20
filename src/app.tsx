6e7b02f6068147f7af46fe9be75dd4eda107d9aa
Tyler Liu <tyler4long@gmail.com>
2024-01-19 22:14:43 -0800

    Remove financials, add more links

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import { Button, Input, Space, Typography, Divider, InputNumber, Select, DatePicker } from 'antd';
 import { auto } from 'manate/react';
 import { init, dispose, registerIndicator } from 'klinecharts';
-import type { IStockFinancialResults, ITickerDetails, ITickerNews } from '@polygon.io/client-js';
+import type { ITickerDetails, ITickerNews } from '@polygon.io/client-js';
 import dayjs from 'dayjs';
 
 import type { Store } from './store';
 import polygon from './polygon';
 import type { TimeSpan } from './types';
-import { financial } from './financials';
 import { formatDateTime } from './utils';
 import vol60 from './indicators/vol-60';
 import ma50200 from './indicators/ma-50-200';
@@ -27,7 +26,6 @@ const App = (props: { store: Store }) => {
   const [to, setTo] = useState(dayjs('2030-01-01'));
   const [ticketDetail, setTickerDetail] = useState(undefined as ITickerDetails | undefined);
   const [tickerNews, setTickerNews] = useState(undefined as ITickerNews | undefined);
-  const [stockFinancials, setStockFinancials] = useState(undefined as IStockFinancialResults | undefined);
 
   const apply = async () => {
     // clean
@@ -61,17 +59,15 @@ const App = (props: { store: Store }) => {
 
     // indicators
     chart.createIndicator('VOL60');
+    // chart.createIndicator('RSI');
     chart.createIndicator('MA50200');
-    chart.createIndicator('EMA1020');
+    // chart.createIndicator('EMA1020');
 
     // ticker detail
     setTickerDetail(await polygon.reference.tickerDetails(ticker));
 
     // ticker news
-    setTickerNews(await polygon.reference.tickerNews({ ticker, limit: 100 }));
-
-    // financial
-    setStockFinancials(await polygon.reference.stockFinancials({ ticker }));
+    setTickerNews(await polygon.reference.tickerNews({ ticker, limit: 16 }));
   };
 
   const render = () => (
@@ -147,7 +143,6 @@ const App = (props: { store: Store }) => {
           ))}
         </ul>
       </Paragraph>
-      {stockFinancials && financial(stockFinancials)}
       <Paragraph>
         <Title level={2}>Quick References</Title>
         <ul>
@@ -157,6 +152,19 @@ const App = (props: { store: Store }) => {
               target="_blank"
             >
               52-week highs
+            </a>
+          </li>
+          <li>
+            <a href="https://www.finviz.com/screener.ashx?v=111&f=ta_sma50_cross200a&ft=3" target="_blank">
+              MA50 cross above MA200
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://finviz.com/screener.ashx?v=111&f=cap_midunder,sh_short_o10,ta_highlow52w_nh&ft=4&o=-volume"
+              target="_blank"
+            >
+              Powerful screener
             </a>
           </li>
         </ul>
